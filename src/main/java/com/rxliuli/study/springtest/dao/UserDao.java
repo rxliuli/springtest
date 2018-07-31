81f6f77e67f2719fcc9d9d7316978038f9fc579f
rxliuli <MJNfYq22Y6j7>
2018-07-31 11:28:55 +0800

    添加实体类 User 和数据库操作类 UserDao

---
+++ src/main/java/com/rxliuli/study/springtest/dao/UserDao.java
@@ -0,0 +1,44 @@
+package com.rxliuli.study.springtest.dao;
+
+import com.rxliuli.study.springtest.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author rxliuli
+ * @date 2018/7/31
+ */
+@Repository
+public class UserDao {
+    private final RowMapper<User> userRowMapper = (rs, rowNum) -> new User(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getBoolean("sex"),
+            rs.getInt("age")
+    );
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * 根据 id 获取一个对象
+     *
+     * @param id id
+     * @return 根据 id 查询到的对象，如果没有查到则为 null
+     */
+    public User get(Integer id) {
+        return jdbcTemplate.queryForObject("select * from user where id = ?", userRowMapper, id);
+    }
+
+    /**
+     * 查询全部用户
+     *
+     * @return 全部用户列表
+     */
+    public List<User> listForAll() {
+        return jdbcTemplate.query("select * from user", userRowMapper);
+    }
+}
---
+++ src/main/java/com/rxliuli/study/springtest/entity/User.java
@@ -0,0 +1,64 @@
+package com.rxliuli.study.springtest.entity;
+
+/**
+ * @author rxliuli
+ * @date 2018/7/31
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Boolean sex;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Boolean sex, Integer age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public User(Integer id, String name, Boolean sex, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public User setSex(Boolean sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public User setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+}
